fad08e032d21506fd981e944a8e57ada1190de3d
Mikkel Malmberg <mikkel@brnbw.com>
2018-01-09 23:54:08 +0100

    Remove all bus listeners before destroying self

--- index.js
+++ index.js
@@ -9,32 +9,35 @@ var defaultOptions = {
 function Dingus(options = {}, w = window) {
   this.options = Object.assign({}, defaultOptions, options);
   this._bus = nanobus("dingus");
-  this._w = w
+  this._w = w;
 
   w.addEventListener("keydown", this._keyDown.bind(this));
 }
 
 Dingus.prototype.on = function(action, cb) {
-  this._bus.on(action, cb)
-}
+  this._bus.on(action, cb);
+};
 
 Dingus.prototype.off = function(action, cb) {
-  this._bus.removeListener(action, cb)
-}
+  this._bus.removeListener(action, cb);
+};
 
 Dingus.prototype.destroy = function() {
+  this._bus.removeAllListeners();
   this._w.removeEventListener("keydown", this._keyDown);
-}
+};
 
-Dingus.prototype._keyDown = function (event) {
+Dingus.prototype._keyDown = function(event) {
   const action = keysToEvents[event.keyCode];
 
   if (!action) return;
 
-  if (this.options.preventDefault) { event.preventDefault(); }
+  if (this.options.preventDefault) {
+    event.preventDefault();
+  }
 
   this._bus.emit(action, event, action);
-}
+};
 
 Dingus.PREV = "prev";
 Dingus.NEXT = "next";
@@ -42,15 +45,15 @@ Dingus.START_STOP = "start-stop";
 Dingus.BLANK_SCREEN = "blank-screen";
 
 var keysToEvents = {
-  8:   Dingus.PREV,        // backspace
-  33:  Dingus.PREV,        // pg up
-  37:  Dingus.PREV,        // left arrow
+  8: Dingus.PREV, // backspace
+  33: Dingus.PREV, // pg up
+  37: Dingus.PREV, // left arrow
 
-  32:  Dingus.NEXT,        // space
-  34:  Dingus.NEXT,        // pg down
-  39:  Dingus.NEXT,        // right arrow
+  32: Dingus.NEXT, // space
+  34: Dingus.NEXT, // pg down
+  39: Dingus.NEXT, // right arrow
 
-  116: Dingus.START_STOP,  // F5
+  116: Dingus.START_STOP, // F5
 
   190: Dingus.BLANK_SCREEN // period (.)
 };
